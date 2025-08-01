8baa749d0a09299b97a426b9a0636a9661b49d1f
Karan Kumbhare <97298886+karankumbhare90@users.noreply.github.com>
2025-08-01 07:10:38 +0530

    Update Sidebar (#4)
    * Setup Global Style Guide
    
    * Update the .yaml file
    
    * Implement Contact Functionality
    
    * Implement Social Links

--- app/_components/AppSidebar.jsx
+++ app/_components/AppSidebar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import TopProfile from './TopProfile'
 import ContactInfo from './ContactInfo'
+import SocialLinks from './SocialLinks'
 
 export default function AppSidebar() {
     return (
         <div className='w-full'>
             <TopProfile />
-            <div className='lg:flex hidden'>
+            <div className='lg:flex hidden lg:pb-5'>
                 <ContactInfo />
+            </div>
+            <div className='py-5 lg:border-t grid grid-cols-1 gap-4'>
+                <SocialLinks />
             </div>
         </div>
     )
--- app/_components/ContactInfo.jsx
+++ app/_components/ContactInfo.jsx
@@ -5,41 +5,44 @@ import { FaLocationDot } from "react-icons/fa6"
 
 export default function ContactInfo() {
 
-    const contactInfo = [
-        {
-            id: 1,
-            label: 'Email',
-            option: 'karankumbhare90@gmail.com',
-            icon: BsEnvelope
-        },
-        {
-            id: 2,
-            label: 'Phone',
-            option: '+91 6352305914',
-            icon: MdCall
-        },
-        {
-            id: 3,
-            label: 'Address',
-            option: 'Surat, Gujarat, India',
-            icon: FaLocationDot
-        },
-    ]
+    const contactInfo = {
+        email: 'karankumbhare90@gmail.com',
+        phone: '+91 6352305914',
+        address: 'Surat, Gujarat, India'
+    }
 
     return (
         <div className='w-full pt-5'>
             <div className='grid grid-cols-1 gap-4'>
-                {contactInfo?.map((item) => (
-                    <div key={item.id} className='flex items-start justify-start gap-2.5'>
-                        <div className='p-2 rounded-full bg-[var(--gray-color-2)]'>
-                            <item.icon className='text-[var(--gray-color-5)]' fontSize={16} />
-                        </div>
-                        <div className='flex flex-col gap-1 items-start justify-center'>
-                            <span className='text-xs text-[var(--gray-color-5)]'>{item.label}</span>
-                            <h5 className='!text-sm !text-[var(--gray-color-6)] !font-normal'>{item.option}</h5>
-                        </div>
+                <div className='flex items-start justify-start gap-2.5'>
+                    <a href={`mailto:${contactInfo?.email}`} className='contact-icon-div group'>
+                        <BsEnvelope fontSize={12} className='contact-icon' />
+                    </a>
+
+                    <div className='flex flex-col gap-1 items-start justify-center'>
+                        <span className='contact-info-lable'>Email</span>
+                        <a href={`mailto:${contactInfo?.email}`} className='contact-info'>{contactInfo.email}</a>
                     </div>
-                ))}
+                </div>
+                <div className='flex items-start justify-start gap-2.5'>
+                    <a href={`tel:${contactInfo?.phone}`} className='contact-icon-div group'>
+                        <MdCall fontSize={12} className='contact-icon' />
+                    </a>
+
+                    <div className='flex flex-col gap-1 items-start justify-center'>
+                        <span className='contact-info-lable'>Phone</span>
+                        <a href={`tel:${contactInfo?.phone}`} className='contact-info'>{contactInfo?.phone}</a>
+                    </div>
+                </div>
+                <div className='flex items-start justify-start gap-2.5'>
+                    <div className='contact-icon-div group'>
+                        <FaLocationDot fontSize={12} className='contact-icon' />
+                    </div>
+                    <div className='flex flex-col gap-1 items-start justify-center'>
+                        <span className='contact-info-lable'>Adress</span>
+                        <div className='contact-info'>{contactInfo?.address}</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
